247c2440281cda109631a983690f07bb66fa552a
Axel ROUQUETTE <axel.rouquette@outlook.fr>
2023-04-19 18:09:54 +0200

    Création du projet

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+function searchBar() {
+    return (
+        <div className="searchBar">
+            <input type="text" placeholder="Search" />
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default searchBar;
---
+++ src/views/Liste.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import searchBar from '../components/SearchBar';
+
+function Liste() {
+    return (
+        <div>
+            <h1>Liste</h1>
+            <searchBar />
+        </div>
+    )
+}
+
+export default Liste;
